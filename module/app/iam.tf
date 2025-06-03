f362330c988f5fefceaccc07a3d0adcffe896a92
Pavani <vemasani.pavani@gmail.com>
2025-06-03 20:25:03 +0530

    create a prometheus role

--- module/app/iam.tf
+++ module/app/iam.tf
@@ -1,5 +1,5 @@
 // create a prometheus role
 resource "aws_iam_role" "test_role" {
   name = "test_role"
-  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.id
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 }
