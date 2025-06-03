8a91c81ae3b08839b0647c21a71267850d443fe9
Pavani <vemasani.pavani@gmail.com>
2025-06-03 20:24:24 +0530

    create a prometheus role

--- module/app/data.tf
+++ module/app/data.tf
@@ -3,3 +3,13 @@ data "aws_ami" "ami" {
   name_regex       = "RHEL-9-DevOps-Practice"
   owners           = ["973714476881"]
 }
+data "aws_iam_policy_document" "instance_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
