5b3316d810a8da8d32b5a9e01265cf0a12509464
Pavani <vemasani.pavani@gmail.com>
2025-06-03 20:42:26 +0530

    inline policy within the role

--- module/app/iam.tf
+++ module/app/iam.tf
@@ -2,6 +2,21 @@
 resource "aws_iam_role" "test_role" {
   name = "prometheus-role"
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+  inline_policy {
+    name = "my_inline_policy"
+
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["ec2:Describe*"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
+
 }
 // create an instance profile
 resource "aws_iam_instance_profile" "test_profile" {
