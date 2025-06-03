fc234e3346704126b1b010aa58ab61ee7cb5323f
Pavani <vemasani.pavani@gmail.com>
2025-06-03 20:45:41 +0530

    create an inline policy

--- module/app/iam.tf
+++ module/app/iam.tf
@@ -2,24 +2,41 @@
 resource "aws_iam_role" "test_role" {
   name = "prometheus-role"
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
-  inline_policy {
-    name = "my_inline_policy"
-
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["ec2:Describe*"]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
-  }
+//  inline_policy {
+//    name = "my_inline_policy"
+//
+//    policy = jsonencode({
+//      Version = "2012-10-17"
+//      Statement = [
+//        {
+//          Action   = ["ec2:Describe*"]
+//          Effect   = "Allow"
+//          Resource = "*"
+//        },
+//      ]
+//    })
+//  }
 
 }
 // create an instance profile
 resource "aws_iam_instance_profile" "test_profile" {
   name = "prometheus-profile"
   role = aws_iam_role.test_role.name
+}
+resource "aws_iam_role_policy" "test_policy" {
+  name = "test_policy"
+  role = aws_iam_role.test_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
 }
