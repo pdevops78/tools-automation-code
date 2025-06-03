4c00559c8efab4926c987af80d57a2a367758b41
Pavani <vemasani.pavani@gmail.com>
2025-06-03 20:32:28 +0530

    assume role policy

--- module/app/iam.tf
+++ module/app/iam.tf
@@ -1,5 +1,10 @@
 // create a prometheus role
 resource "aws_iam_role" "test_role" {
-  name = "test_role"
+  name = "prometheus-role"
   assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
+}
+// create an instance profile
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "prometheus-profile"
+  role = aws_iam_role.test_role.name
 }
