810a9b9372b181663096bc70f40ac5240a7e6eab
Pavani <vemasani.pavani@gmail.com>
2025-06-03 20:36:44 +0530

    add instance profile to ec2 instance

--- module/app/main.tf
+++ module/app/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.ami.id
   instance_type = var.instance_type
-
+  iam_instance_profile = aws_iam_instance_profile.test_profile.name
   instance_market_options {
     market_type = "spot"
     spot_options {
