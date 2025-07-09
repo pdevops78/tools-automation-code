c172db73c7bf20e6ef43f0295fe600684da6b2c5
Your Name <vemasani.pavani@gmail.com>
2025-07-09 12:20:53 +0530

    tools

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "tools" {
   default = {
-    prometheus = {
-      instance_type = "t2.micro"
-    }
-    grafana = {
-      instance_type = "t2.micro"
-    }
-    vault = {
-      instance_type = "t2.micro"
-    }
+#     prometheus = {
+#       instance_type = "t2.micro"
+#     }
+#     grafana = {
+#       instance_type = "t2.micro"
+#     }
+#     vault = {
+#       instance_type = "t2.micro"
+#     }
+     sonarqube = {
+       instance_type = "t3.micro"
+         }
   }
 }
 variable "env"{}
